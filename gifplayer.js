ff08f1cc3387cacfdd057c68bf954193792f834a
Sinan Ozer <sinan.ozer7@protonmail.com>
2020-12-28 16:56:41 -0800

    re-working page structure

--- gifplayer.js
+++ gifplayer.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
   $(".column#gif").hover(
     function () {
-      var src = $(this).attr("src");
+      let src = $(this).attr("src");
       $(this).attr("src", src.replace(/\.png$/i, ".gif"));
     },
     function () {
-      var src = $(this).attr("src");
+      let src = $(this).attr("src");
       $(this).attr("src", src.replace(/\.gif$/i, ".png"));
     });
 
   $(".column2#gif").hover(
     function () {
-      var src = $(this).attr("src");
+      let src = $(this).attr("src");
       $(this).attr("src", src.replace(/\.png$/i, ".gif"));
     },
     function () {
-      var src = $(this).attr("src");
+      let src = $(this).attr("src");
       $(this).attr("src", src.replace(/\.gif$/i, ".png"));
     });
 
